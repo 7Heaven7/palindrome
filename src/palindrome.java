0233c134f9203598c96bf524e15a034251b03602
Kidda.KS <kidda.ks@gmail.com>
2022-07-21 22:32:57 +0700

    refactor variable wording and add input

--- src/palindrome.java
+++ src/palindrome.java
@@ -1,32 +1,36 @@
+import java.util.Scanner;
+
 public class palindrome {
     static int maxPalindromeLength;
     static int firstPalindromeIndex;
 
     public static void main(String[] args) {
-        String word = "l-ANNA@SUGAGA_ANMNA-";
-
-        System.out.println("Longest Palindrome is : " + findLongestPalindrome(word));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the String : ");
+        String inputString = scanner.next();
+        System.out.println("Longest Palindrome is : " + findLongestPalindrome(inputString));
+        scanner.close();
     }
 
-    public static String findLongestPalindrome(String word) {
-        int wordLength = word.length();
-        if (wordLength == 0) {
-            return "the word is empty.";
+    public static String findLongestPalindrome(String string) {
+        int stringLength = string.length();
+        if (stringLength == 0) {
+            return "the string is empty.";
         }
-        if (wordLength == 1) {
-            return word;
+        if (stringLength == 1) {
+            return string;
         }
-        for (int index = 0; index < wordLength; index++) {
-            findPalindromeLength(word, index, index);
-            findPalindromeLength(word, index, index + 1);
+        for (int index = 0; index < stringLength; index++) {
+            findPalindromeLength(string, index, index);
+            findPalindromeLength(string, index, index + 1);
         }
 
-        return word.substring(firstPalindromeIndex, firstPalindromeIndex + maxPalindromeLength);
+        return string.substring(firstPalindromeIndex, firstPalindromeIndex + maxPalindromeLength);
     }
 
-    public static void findPalindromeLength(String word, int startIndex, int endIndex) {
-        while (startIndex >= 0 && endIndex < word.length()
-                && word.charAt(startIndex) == word.charAt(endIndex)) {
+    public static void findPalindromeLength(String string, int startIndex, int endIndex) {
+        while (startIndex >= 0 && endIndex < string.length()
+                && string.charAt(startIndex) == string.charAt(endIndex)) {
             startIndex--;
             endIndex++;
         }
